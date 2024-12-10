6ba3934b2ba923f897320ac098b7334c6cee682d
chirley <chirleyfatec@gmail.com>
2024-12-10 15:11:29 -0300

    a

--- src/INICIANTE/URI1020.java
+++ src/INICIANTE/URI1020.java
@@ -1,9 +1,17 @@
 package INICIANTE;
 
+import java.time.Duration;
+import java.util.Scanner;
+
 public class URI1020 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int num = sc.nextInt();
+		
+		Duration horas = Duration.ofSeconds(num);
+		
+		System.out.println(horas.toHours()+":"+horas.toMinutesPart()+":"+horas.toSecondsPart());
 
 	}
 
